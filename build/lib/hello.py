1fc4c8930fd5eb15c73ecee3a06193c0bb4f8801
Bruno Macieira <macieira.bruno@gmail.com>
2023-05-22 15:14:33 +0100

    First Commit

---
+++ build/lib/hello.py
@@ -0,0 +1,7 @@
+def say_hi(nome):
+    print(f'Hi, my name is {nome}.')
+
+def city():
+    print('Abrantes')
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Bruno Macieira',
+      author_email='macieira.bruno@gmail.com',
+      url='The URL project - github for example',
+      py_modules=['hello'],
+     )
